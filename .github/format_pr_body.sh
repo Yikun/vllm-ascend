788eb18fc274f89523a6e640298dfc9037eb3ea3
Yikun Jiang <yikunkero@gmail.com>
2025-07-15 19:17:04 +0800

    Remove redundant empty lines in commit msg
    Signed-off-by: Yikun Jiang <yikunkero@gmail.com>

--- .github/format_pr_body.sh
+++ .github/format_pr_body.sh
@@ -32,7 +32,8 @@ OLD=/tmp/orig_pr_body.txt
 NEW=/tmp/new_pr_body.txt
 
 gh pr view --json body --template "{{.body}}" "${PR_NUMBER}" > "${OLD}"
-cp "${OLD}" "${NEW}"
+# Remove redundant empty lines
+uniq "${OLD}" > "${NEW}"
 
 # Remove notes in pr description and add vLLM version and commit
 sed -i '/<!--/,/-->/d' "${NEW}"
