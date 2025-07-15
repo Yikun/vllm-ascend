f0d4b93230b44c4d31615d2efe519487ae9ced5b
Yikun Jiang <yikunkero@gmail.com>
2025-07-15 19:44:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #
 # Copyright (c) 2025 Huawei Technologies Co., Ltd. All Rights Reserved.
-# Copyright 2023 The vLLM team.
+# Copyright 2023 The vLLM team. test
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
