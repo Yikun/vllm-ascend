3ad4d6db00a1ca9de57cd887485e9e69d745af6b
Yikun Jiang <yikunkero@gmail.com>
2025-07-16 13:49:14 +0800

    Update ascend_config.py

--- vllm_ascend/ascend_config.py
+++ vllm_ascend/ascend_config.py
@@ -1,6 +1,6 @@
 #
 # Copyright (c) 2025 Huawei Technologies Co., Ltd. All Rights Reserved.
-# This file is a part of the vllm-ascend project. test
+# This file is a part of the vllm-ascend project. test2
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
